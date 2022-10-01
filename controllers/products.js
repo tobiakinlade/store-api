bc9183a5a5b4db9365a4bd0dd4dde01f1aeacc78
tobiakinlade <tobi.akinlade@yahoo.com>
2022-10-01 16:43:35 +0100

    querying the features in the DB

--- controllers/products.js
+++ controllers/products.js
@@ -6,8 +6,20 @@ const getAllStaticProducts = async (req, res) => {
 }
 
 const getAllProducts = async (req, res) => {
-  const products = await Product.find(req.query)
-  res.status(200).json({ products })
+  const { featured, company, name } = req.query
+  const queryObject = {}
+
+  if (featured) {
+    queryObject.featured = featured === 'true' ? true : false
+  }
+  if (company) {
+    queryObject.company = company
+  }
+  if (name) {
+    queryObject.name = { $regex: name, $options: 'i' }
+  }
+  const products = await Product.find(queryObject)
+  res.status(200).json({ products, nbHits: products.length })
 }
 
 module.exports = {
